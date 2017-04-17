ccec0ecf8eaf9b734d40fd4ca4286fd410eabb17
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-17 08:19:20 -0500

    Update utils.h

--- submission/utils.h
+++ submission/utils.h
@@ -1,4 +1,6 @@
-#pragma once
+/* *
+ *  CS296 Honor Project
+ */
 
 ssize_t read_from_socket(int socket, char *buffer, size_t count);
 ssize_t write_to_socket(int socket, const char *buffer, size_t count);
