f21a7bef4a4a922fdd2aef250425ac057e100184
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-18 16:33:00 -0500

    Update utils.h

--- submission/utils.h
+++ submission/utils.h
@@ -1,6 +1,19 @@
 /* *
  *  CS296 Honor Project
  */
-
+// #include "common.h"
+#include <netdb.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 ssize_t read_from_socket(int socket, char *buffer, size_t count);
 ssize_t write_to_socket(int socket, const char *buffer, size_t count);
