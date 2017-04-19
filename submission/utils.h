3f095375eaf02a19d7f1c49d0f1cc140bb460956
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-19 11:08:03 -0500

    Update utils.h

--- submission/utils.h
+++ submission/utils.h
@@ -15,5 +15,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
+// int secret[8] = { 125,126,173,225,233,241,296,374 };
 ssize_t read_from_socket(int socket, char *buffer, size_t count);
 ssize_t write_to_socket(int socket, const char *buffer, size_t count);
+void encrypt(char* origin,size_t len,int * key,int key_len);
