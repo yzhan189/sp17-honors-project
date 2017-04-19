6246ad459974cdc7da2f2629798b8026e7597560
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-19 11:28:49 -0500

    Update encrypt.c

--- submission/encrypt.c
+++ submission/encrypt.c
@@ -1,6 +1,8 @@
 /* *
  *  CS296 Honor Project
  */
+
+
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -8,11 +10,5 @@
 #include <string.h>
 #include <unistd.h>
 
-int secret[8] = { 125,126,173,225,233,241,296,374 };
 
-void encrypt(char* origin,size_t len,int * key,int key_len){
-    int i;
-    for(i = 0; i <len; i++){
-        origin[i] = origin[i]^key[i%key_len];
-    }
-}
+void encrypt(char* origin,size_t len,int * key,int key_len);
