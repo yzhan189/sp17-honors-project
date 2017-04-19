20f0ae2661feaa6e218503a874a4e0862de41e79
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-18 20:26:10 -0500

    Update unit_client.c

--- submission/unit_client.c
+++ submission/unit_client.c
@@ -14,10 +14,6 @@
 #include <unistd.h>
 
 static volatile int serverSocket;
-// static pthread_t threads[2];
-
-// void *write_to_server(void *arg);
-// void *read_from_server(void *arg);
 void close_program(int signal);
 ssize_t write_to_server(int socket, const char* url, size_t count){
   printf("SENDING: %s\n", url);
@@ -57,43 +53,31 @@ void close_server_connection() {
  * Returns integer of valid file descriptor, or exit(1) on failure.
  */
 int connect_to_server(const char *host, const char *port, const char *url) {
-    /*QUESTION 1*/
-    // 1) What is a socket?
-    /*QUESTION 2*/
-    // 2) What is the difference between the domains AF_INET and AF_UNIX?
-    /*QUESTION 3*/
-    // 3) What is the difference between SOCK_STREAM and SOCK_DGRAM?
+    
 
     int s;
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-
-    /*QUESTION 4*/
-    // 4) Why is it important to clear all the values the addrinfo struct before using it in getaddrinfo?
     struct addrinfo hints, *result;
     memset(&hints, 0, sizeof(struct addrinfo));
 
 
-    /*QUESTION 5*/
-    // 5) What are ai_family and ai_socktype?
+
     hints.ai_family = AF_INET; /* IPv4 only */
     hints.ai_socktype = SOCK_STREAM; /* TCP */
-    /*QUESTION 6*/
-    // 6) What does getaddrinfo do?
+
     s = getaddrinfo(host, port, &hints, &result);
     if (s != 0) {
             fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
             exit(1);
     }
-    /*QUESTION 7*/
-    // 7) What does connect do?
+
     int ret = connect(serverSocket, result->ai_addr, result->ai_addrlen);
     if(ret == -1){
       perror("connect");
       exit(1);
     }
-    //send info
-    //char *buf = "GET / HTTP/1.0\r\n\r\n";
+
     size_t count = strlen(url);
     printf("SENDING: %s\n", url);
     printf("===\n");
@@ -113,6 +97,12 @@ int connect_to_server(const char *host, const char *port, const char *url) {
     char resp[1000];
     int len = read(serverSocket , resp, 999);
     resp[len] = '\0';
+    /*
+     *  
+     *  decrption goes here!
+     *
+     *
+     */
     printf("%s\n", resp);
 
     return ret;
@@ -125,9 +115,6 @@ int connect_to_server(const char *host, const char *port, const char *url) {
  */
 void close_program(int signal) {
     if (signal == SIGINT) {
-        // pthread_cancel(threads[0]);
-        // pthread_cancel(threads[1]);
-        // close_chat();
         close_server_connection();
     }
 }
@@ -142,9 +129,6 @@ int main(int argc, char **argv) {
 
 
     signal(SIGINT, close_program);
-    // create_windows(output_filename);
-    // atexit(destroy_windows);
-
     serverSocket = connect_to_server(argv[1], argv[2], argv[3]);
 
     return 0;
