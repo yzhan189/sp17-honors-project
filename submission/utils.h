fbb0ef23edcf9209a7cb0b4a2faaf54f5498565d
yzhan189 <yzhan189@illinois.edu>
2017-04-23 16:27:17 -0500

    Update utils.h

--- submission/utils.h
+++ submission/utils.h
@@ -18,4 +18,4 @@
 // int secret[8] = { 125,126,173,225,233,241,296,374 };
 ssize_t read_from_socket(int socket, char *buffer, size_t count);
 ssize_t write_to_socket(int socket, const char *buffer, size_t count);
-void encrypt(char* origin,size_t len,int * key,int key_len);
+void encrypt_(char* origin,size_t len,int * key,int key_len);
