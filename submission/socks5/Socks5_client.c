7c34e266c25a34f5f8a5f1842d6eb372f47df3a9
ruiqili2 <ruiqili2@users.noreply.github.com>
2017-04-29 19:55:00 -0500

    Update Socks5_client.c

--- submission/socks5/Socks5_client.c
+++ submission/socks5/Socks5_client.c
@@ -1,9 +1,9 @@
 /* *
  *  CS296 Honor Project
  */
-#include "utils.h"
-#include "encrypt.h"
-
+// #include "utils.h"
+// #include "encrypt.h"
+#include "Socks5.h"
 #include <errno.h>
 #include <netdb.h>
 #include <pthread.h>
@@ -14,17 +14,20 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-int secret[8] = { 125,126,173,225,233,241,296,374 };
-enum {SUCCESS, AUTH_ERROR, VERSION_ERROR} status
+#include <netinet/in.h>
+#include <arpa/inet.h>
+//curl localhost:1080 -u admin http://www.google.com
+
+
 static volatile int serverSocket;
 void close_program(int signal);
-ssize_t write_to_server(int socket, const char* url, size_t count){
-  printf("SENDING: %s\n", url);
+ssize_t write_to_server(int socket, const char* target_domain, size_t count){
+  printf("SENDING: %s\n", target_domain);
   printf("===\n");
   ssize_t bytewrite = 0;
   ssize_t totalwrite = 0;
   while(count > 0) {
-    bytewrite = write(serverSocket , url, strlen(url));
+    bytewrite = write(serverSocket , target_domain, strlen(target_domain));
     if(bytewrite == -1) {
       return -1;
     }
@@ -41,13 +44,13 @@ void read_from_client(int socket);
  * Called by close_program upon SIGINT.
  */
 void close_server_connection() {
-    // Your code here
-    shutdown(serverSocket, SHUT_RDWR);
-    close(serverSocket);
+	// Your code here
+	shutdown(serverSocket, SHUT_RDWR);
+	close(serverSocket);
 }
 
 /**
- * Sets up a connection to a chatroom server and returns
+ * Sets up a connection to a proxy server and returns
  * the file descriptor associated with the connection.
  *
  * host - Server to connect to.
@@ -55,151 +58,148 @@ void close_server_connection() {
  *
  * Returns integer of valid file descriptor, or exit(1) on failure.
  */
-int connect_to_server(const char *host, const char *port, const char *username,
-                      const char *password, const char *url) {
+int connect_to_server(const char* host, const char* port, const char *username,
+                      const char *password, const char *target_domain, const char* target_port) {
+
+	/**
+	 *	create socket
+	 */
+	int s;
+
+	/**
+	 *	connect to server
+	 */
+	struct addrinfo server_addr, *result;
+	bzero(&server_addr, sizeof(struct addrinfo));
+
+	server_addr.ai_family = AF_INET; /* IPv4 only */
+	server_addr.ai_socktype = SOCK_STREAM; /* TCP */
+
+	s = getaddrinfo(host, port, &server_addr, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		return SOCKS5_ERROR_GETADDRINFO;
+	}
+
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	int ret = connect(serverSocket, result -> ai_addr, result -> ai_addrlen);
+	if(ret == -1){
+		perror("connect to proxy");
+		return SOCKS5_ERROR_CONNECT;
+	}
+
+	fprintf(stderr, "connect successfully\n");
+	/**
+	 *	connect successfully
+	 *	start verify the SOCKS version
+	 */
+	int i;
+	/**
+	*  GET AUTH METHOD
+	*/
+	char buffer[BUFF_SIZE] = {0};
+	char *pen = buffer;
+	*pen ++ = 0x05;// version
+	*pen ++ = 2;// support method count
+	*pen ++ = 0x02;// auth mode
+	*pen ++ = 0x00;//
+
+	fprintf(stderr, "%u\n", (unsigned)buffer[0]);
+	fprintf(stderr, "%u\n", (unsigned)buffer[1]);
+	fprintf(stderr, "%u\n", (unsigned)buffer[2]);
+	fprintf(stderr, "%u\n", (unsigned)buffer[3]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[4]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[5]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[6]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[7]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[8]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[9]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[10]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[11]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[12]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[13]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[14]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[15]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[16]);
+	// fprintf(stderr, "%u\n", (unsigned)buffer[17]);
+	// fprintf(stderr, "%zu: %s\n", (size_t)(pen - buffer), buffer);
+
+	send(serverSocket, buffer, pen - buffer, 0);
+	fprintf(stderr, "%s reached line %d in %s\n", __FILE__,__LINE__,__FUNCTION__);
+	fprintf(stderr, "METHOD: recv %zu byte from server\n", strlen(buffer));
+
+	recv(serverSocket, buffer, 2, 0);
+
+	fprintf(stderr, "resp: %s\n", buffer);
+	if((unsigned int)*(buffer + 1) == 0xFF) {
+		perror("version not support");
+		exit(-1);
+	}
+	else if((unsigned int)*(buffer + 1) == 0x02) {
+		//version supported
+		fprintf(stderr, "version supported, sending username & password for AUTH\n");
+
+		// send usrname
+		pen = buffer;
+		*pen ++ = 0x01;// version
+		// pen ++;
+		size_t username_len = strlen(username);
+		*pen ++ = username_len;
+		strcpy(pen, username);
+		fprintf(stderr, "username: %s\n",pen );
+		pen += username_len;
+		//send password
+
+		size_t password_len = strlen(password);
+		*pen ++ = password_len;
+		strcpy(pen, password);
+		fprintf(stderr, "password: %s\n",pen);
+		pen += password_len;
 
 
-    int s;
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		fprintf(stderr, "AUTH: send %u byte from server = [%s]\n", (unsigned)(pen - buffer), buffer);
 
-    struct sockaddr_in server_addr;
-    bzero(&server_addr, sizeof(struct sockaddr_in));
-    saddr.sin_port = htons(1080);
+		send(serverSocket, buffer, pen - buffer, 0);
+		fprintf(stderr, "%s reached line %d in %s\n", __FILE__,__LINE__,__FUNCTION__);
+		fprintf(stderr, "AUTH: recv %zu byte from server\n", strlen(buffer));
+		recv(serverSocket, buffer, 2, 0);
 
+		if((unsigned int)*(buffer + 1) != 0) {
+			perror("Authentication");
+			exit(SOCKS5_ERROR_AUTH);
+		}
+		fprintf(stderr, "CLIENT AUTH PASS!\n");
+		// Authentication pass
+		pen = buffer;
+		*pen ++ = 5;
 
-    int ret = connect(serverSocket, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if(ret == -1){
-      perror("connect to proxy");
-      exit(1);
-    }
+		*pen ++ = 1;
 
+		*pen ++ = 0;
 
-    /**
-     *  GET AUTH METHOD
-     */
-    char buf[256];
-    char *pen = buf;
-    pen ++ = 5;
-    pen ++ = 2;
-    pen ++ = 0x00;
-    pen ++ = 0x02;
-    send(serverSocket, pen, buf - pen, 0);
-    recv(serverSocket, buf, 2, 0);
+		*pen ++ = 3; //DOMAIN
 
-    if(buf[1] == 0xFF) {
-      perror("version not support");
-      exit(-1);
-    }
-    else if(buf[1] == 0x02) {
-      //version supported
-      // send usrname
-      pen = buf;
-      pen ++ = 1;
-      size_t username_len = strlen(username);
-      pen ++ = username_len;
-      strcpy(pen, username);
-      pen += username_len;
-      //send password
-      size_t password_len = strlen(password);
-      ptr ++ = password_len;
+		/**
+		* get the address of target destination
+		*/
+		*pen ++ = strlen(target_domain);
+		strncpy(pen, target_domain, strlen(target_domain));
+		pen += strlen(target_domain);
+		strncpy(pen, target_port, strlen(target_port));
+		pen += strlen(target_port);
 
-      send(serverSocket, buf, pen - buf);
-      recv(serverSocket, buf, 2);
+		fprintf(stderr, "AUTH: send %u byte to server\n", (unsigned)(pen - buffer));
+		send(serverSocket, buffer, pen-buffer, 0);
+		recv(serverSocket, buffer, 4, 0);
+		fprintf(stderr, "AUTH: recv %zu byte from server\n", strlen(buffer));
 
-      if(buf[1] != 0) {
-        perror("Authentication");
-        exit(AUTH_ERROR);
-      }
-      // Authentication pass
-      pen = buffer;
-      pen++ = 5;  //socks verison
-      pen++ = 1;  //sending connect cmd
-      pen++ = 0;
-      pen++ = 1;
-      /**
-       * get the address of target destination
-       */
-      struct addrinfo hints, *result;
-      memset(&hints, 0, sizeof(struct addrinfo));
+		if((unsigned int)*(buffer + 1) == 0x00) {
+			fprintf(stderr, "Success!" );
+		}
 
-      hints.ai_family = AF_INET; /* IPv4 only */
-      hints.ai_socktype = SOCK_STREAM; /* TCP */
-
-      s = getaddrinfo(host, port, &hints, &result);
-      if (s != 0) {
-             fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-             exit(1);
-      }
-
-      struct addrinfo *p;
-      char host[256],service[256];
-
-      for(p = result; p != NULL; p = p->ai_next) {
-
-        getnameinfo(p->ai_addr, p->ai_addrlen, host, sizeof(host), service, sizeof(service), NI_NUMERICHOST);
-        puts(host);
-      }
-
-      freeaddrinfo(infoptr);
-      memcpy(pen, )
-
-
-
-    }
-
-    char *msg = calloc(1, strlen(url) + 1);
-    strcpy(msg, url);
-    msg[strlen(url)] = '\0';
-
-    fprintf(stderr, "BEFORE ENCRPTION:[%s]\n> strlen(url): [%lu]\n> strlen(msg): [%lu]\n",msg,strlen(url),strlen(msg));
-    encrypt(msg,strlen(msg),secret,8);
-    msg[strlen(url)] = '\0';
-    fprintf(stderr, "AFTER ENCRPYTION:[%s]\n> strlen(url): [%lu]\n> strlen(msg): [%lu]\n",msg,strlen(url),strlen(msg));
-    // encrypt(msg,strlen(msg),secret,8);
-    // msg[strlen(url)] = '\0';
-    // fprintf(stderr, "DECRPYTION:[%s]\n> strlen(url): [%lu]\n> strlen(msg): [%lu]\n",msg,strlen(url),strlen(msg));
-    // if(strcmp(msg, url)) {
-    //   perror("encrypt\n");
-    // };
-
-
-    size_t count = strlen(msg);
-    printf("SENDING [%lu] bytes: %s\n", strlen(url), msg);
-    printf("===\n");
-    // return 0;
-    ssize_t bytewrite = 0;
-    ssize_t totalwrite = 0;
-    while(count > 0) {
-      bytewrite = write(serverSocket , msg, strlen(msg));
-      if(bytewrite == -1) {
-        return -1;
-      }
-      count -= bytewrite;
-      totalwrite += bytewrite;
-    }
-    fprintf(stderr, "total sent: %zu\n", totalwrite);
-    shutdown(serverSocket, SHUT_WR);
-    // return 0;
-    // sleep(3);
-    //read response
-    char resp[1000];
-    int len = read(serverSocket , resp, 999);
-    resp[len] = '\0';
-    encrypt(resp,strlen(resp),secret,8);
-    resp[strlen(resp)] = '\0';
-    fprintf(stderr, "## DECRPYTION SERVER RESPONSE:[%s](lem: %lu)\n",resp,strlen(resp));
-    // if(strcmp(resp, url)) {
-    //   perror("decrypt\n");
-    // };
-    /*
-     *
-     *  decrption goes here!
-     *
-     *
-     */
-    printf("%s\n", resp);
-
-    return ret;
+	}
+	return ret;
 }
 
 
@@ -215,15 +215,21 @@ void close_program(int signal) {
 
 int main(int argc, char **argv) {
 
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <address> <port> <url>\n",
+    if (argc != 7) {
+        fprintf(stderr, "Usage: %s <host> <port> <username> <password> <target_domain> <target_port>\n",
                 argv[0]);
         exit(1);
     }
 
 
     signal(SIGINT, close_program);
-    serverSocket = connect_to_server(argv[1], argv[2], argv[3]);
+    serverSocket = connect_to_server(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
 
+    char resp[1000];
+    int len = 0;
+    while ((len = read (serverSocket, resp, 999)) > 0) {
+	    resp[len] = '\0';
+	    printf("%s\n", resp);
+    }
     return 0;
 }
